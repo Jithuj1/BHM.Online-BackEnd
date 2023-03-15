c79af779cd6b1cbbccba0a25874145c5db9a5a53
Jithu Jacob <jithujacob73@gmail.com>
2023-03-15 10:41:53 +0530

    cors solved

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -73,9 +73,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000'
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'BackEnd.urls'
 
